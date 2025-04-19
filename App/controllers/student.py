f081e312cd2ab10cec76b88952c8c0023c419054
0xNick404 <ngrimestt@gmail.com>
2025-04-19 18:07:00 -0400

    Update student.py

--- App/controllers/student.py
+++ App/controllers/student.py
@@ -1,7 +1,7 @@
 from App.models import Student
 from App import db
 
-def applyTointernship(student_id, internship_id):
+def apply_internship(student_id, internship_id):
     student = Student.query.get(student_id)
     if student:
         student.internship_id = internship_id
