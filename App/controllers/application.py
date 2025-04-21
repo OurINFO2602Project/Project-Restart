54a0dedbe9e35b78520a6a67a3287c02c74952c3
Vramaz <ramadhinvinai@gmail.com>
2025-04-20 21:22:34 -0400

    new changes

--- App/controllers/application.py
+++ App/controllers/application.py
@@ -4,9 +4,8 @@ from App.database import db
 def create_application(internship_id, student_id, degree, gpa, graduation_year, resume_url):
     internship = Internship.query.get(internship_id)
     student = Student.query.get(student_id)
-    if not internship or not student:
-        return None
-    application = Application(internship=internship, student=student, degree=degree, gpa=gpa, graduation_year=graduation_year, url=resume_url)
-    db.session.add(application)
+    if internship or student:
+        application = Application(internship=internship, student=student, degree=degree, gpa=gpa, graduation_year=graduation_year, url=resume_url)
+        db.session.add(application)
     db.session.commit()
     return application
--- App/views/student.py
+++ App/views/student.py
@@ -8,6 +8,7 @@ from App import db
 student_views = Blueprint('student_views', __name__, template_folder='../templates')
 
 @student_views.route('/apply/<int:internship_id>', methods=['GET', 'POST'])
+@jwt_required()
 def apply_to_internship(internship_id):
     internship = Internship.query.get(internship_id)
     if request.method == 'POST':
@@ -26,7 +27,7 @@ def apply_to_internship(internship_id):
         )
         
         flash('Application submitted!')
-        return redirect(url_for('student_views.home'))
+        return redirect(url_for('student_views.apply_to_internship', internship_id=internship_id))
     
     elif request.method == 'GET':
         return render_template('student.html', internships=Internship.query.all(), current_user=current_user, selected_internship=internship)
