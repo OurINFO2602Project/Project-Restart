14eeed801533a9dbea26f7ccfbf63ed1df0ddd6a
Mikel Maharaj <151253044+MikelMaharaj@users.noreply.github.com>
2025-04-20 21:26:46 -0400

    Update application.py

--- App/controllers/application.py
+++ App/controllers/application.py
@@ -1,11 +1,16 @@
-from App.models import Application, Internship, Student
-from App.database import db
+from App.models import Application
+from App import db
 
-def create_application(internship_id, student_id, degree, gpa, graduation_year, resume_url):
-    internship = Internship.query.get(internship_id)
-    student = Student.query.get(student_id)
-    if internship or student:
-        application = Application(internship=internship, student=student, degree=degree, gpa=gpa, graduation_year=graduation_year, url=resume_url)
-        db.session.add(application)
+def create_application(internship_id, name, email, degree, gpa, graduation_year, resume_url):
+    application = Application(
+        internship_id=internship_id,
+        name=name,
+        email=email,
+        degree=degree,
+        gpa=gpa,
+        graduation_year=graduation_year,
+        resume_url=resume_url
+    )
+    db.session.add(application)
     db.session.commit()
     return application
